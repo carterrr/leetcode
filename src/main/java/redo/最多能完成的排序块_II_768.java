ba4d566d3c47ac43828e9da7d9353b4d03d04fd4
carterrr <78474978@qq.com>
2022-04-06 14:48:26 +0000

    add src/main/java/redo/最多能完成的排序块_II_768.java.

---
+++ src/main/java/redo/最多能完成的排序块_II_768.java
@@ -0,0 +1,19 @@
+public class 最多能完成的排序块_II_768{}
+// 比前面块最大值大的数字成一块  如果出现小的 看是前面哪几个最大值和这个小的数字合并成一块
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+       Deque<Integer> stack = new LinkedList<>();
+       for(int i : arr) {
+           if(!stack.isEmpty() && i < stack.peek()) {
+               int top = stack.pop();
+               while(!stack.isEmpty() && i < stack.peek())  {
+                   stack.pop();
+               }
+               stack.push(top);
+           } else {
+               stack.push(i);
+           }
+       }
+       return stack.size();
+    }
+}
