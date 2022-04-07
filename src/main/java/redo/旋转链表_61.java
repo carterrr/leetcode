254fa3fc442d515ef6d6ec1cecdddad19363bb21
carterrr <78474978@qq.com>
2022-04-07 10:04:11 +0000

    add src/main/java/redo/旋转链表_61.java.

---
+++ src/main/java/redo/旋转链表_61.java
@@ -0,0 +1,38 @@
+
+public class 旋转链表_61{}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0) return head;
+        int l = 1 ;
+        ListNode dummyHead = head;
+        while(head.next != null) {
+            head = head.next;
+            l++;
+        }
+        ListNode tail = head;
+        k = k % l;
+        if(k == 0) return dummyHead;
+        l -= k;
+
+        head = dummyHead;
+        while(--l > 0) {
+            head = head.next;
+        }
+
+        ListNode newHead = head.next;
+        head.next = null;
+        tail.next = dummyHead;
+        return newHead;
+    }
+}
