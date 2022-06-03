8f73fd706fe023f0dad7391758c41899e582d077
carterrr <78474978@qq.com>
2022-06-03 03:06:16 +0000

    add src/main/java/redo/零钱兑换II_518.java.

---
+++ src/main/java/redo/零钱兑换II_518.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        //两层循环coin在外算组合  coin在内算排列
+        for(int coin : coins) {
+        for(int i = 1; i <= amount; i++) {// 外层amount 内层coin时 会对内层coin重复计算  如和为 3  算了 1 2  也算了 2 1  
+                if(coin <= i) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+            
+        }
+        return dp[amount];
+    }
+}
